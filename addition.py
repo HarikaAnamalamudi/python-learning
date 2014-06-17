a7fe247ca8a531ad2f5049c208b0889a6b7f633f
Harika Anamalamudi <harika.anamalamudi@codemenders.com>
2014-06-18 00:13:37 +0530

    enhancing the appearance of the output

--- addition.py
+++ addition.py
@@ -6,15 +6,15 @@ def mul(x,y):
 	return x*y
 def div(x,y):
 	return x/y
-print ("Choose the operation")
+print ("Choose the operation:")
 print ("1.+")
 print ("2.-")
 print ("3.*")
 print ("4./")
 
-choice =input ("enter choice")
-num1= int(input("enter the first operand"))
-num2= int(input("enter the second operand"))
+choice =input ("enter choice:")
+num1= int(input("enter the first operand:" ""))
+num2= int(input("enter the second operand:" ""))
 
 if choice=='1':
 	print(num1,"+",num2,"=", add(num1,num2))
@@ -25,4 +25,4 @@ elif choice=='3':
 elif choice=='4':
 	print(num1,"/",num2,"=", div(num1,num2))
 else:
-	print("error!operator not correct")
+	print("error!operator not correct:")
