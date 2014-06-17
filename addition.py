6355ea151b36868685667f5d53182d886f768457
Harika Anamalamudi <harika.anamalamudi@codemenders.com>
2014-06-18 00:07:12 +0530

    python script which takes inputs from users and performs desired operations

---
+++ addition.py
@@ -0,0 +1,20 @@
+print ("Choose the operation")
+print ("1.+")
+print ("2.-")
+print ("3.*")
+print ("4./")
+
+choice =input ("enter choice")
+num1= int(input("enter the first operand"))
+num2= int(input("enter the second operand"))
+
+if choice=='1':
+	print(num1,"+",num2,"=", num1+num2)
+elif choice=='2':
+	print(num1,"-",num2,"=", num1-num2)
+elif choice=='3':
+	print(num1,"*",num2,"=", num1*num2)
+elif choice=='4':
+	print(num1,"/",num2,"=", num1/num2)
+else:
+	print("error!operator not correct")
