fe63633a6cc42dd9c702640047244c2f66fff3bb
Harika Anamalamudi <harika.anamalamudi@codemenders.com>
2014-06-19 17:48:53 +0530

    changes to functions

--- class.py
+++ class.py
@@ -5,11 +5,11 @@ class Calculator:
 	
 	def add(self,x,y):
 		return x+y
-	def sub(x,y):
+	def sub(self,x,y):
 		return x-y
-	def mul(x,y):
+	def mul(self,x,y):
 		return x*y
-	def div(x,y):
+	def div(self,x,y):
 		return x/y
 c= Calculator()
 print ("choose the operation:")
