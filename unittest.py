9021c2a42b66f35c93be9b1bdc5478b54b535115
Nimika Keshri <nimika.keshri@codemenders.com>
2014-06-26 18:19:32 +0300

    Instantiate class variable in unit test

--- unittest.py
+++ unittest.py
@@ -1,26 +1,32 @@
-from class import *
+
+from classs import Calculator2 as Calc
 import unittest
 
-class TestCalculator(unittest.TestCase):
-	def SetUp(self):
+class TestCalculator (unittest.TestCase):
+	def setUp(self):
+		print ("STARTING TESTCASE")
+		self.c = Calc()
 		pass
+	def tearDown(self):
+		print ("ENDING TESTCASE")
+		
 	def testadd(self):
-		self.assertEqual(add(2,2),4,'ok')
-		self.assertEqual(add(-3,2),-1,'ok')
-		self.assertEqual(add(2,-1),1,'ok')
-		self.assertEqual(add(-2,3),1,'ok')
+		self.assertEqual(self.c.add(2,2),4,'ok')
+		self.assertEqual(self.c.add(-3,2),-1,'ok')
+		self.assertEqual(self.c.add(2,-1),1,'ok')
+		self.assertEqual(self.c.add(-2,3),1,'ok')
 	def testsub(self):
-		self.assertEqual(sub(2,1),1,'ok')
-		self.assertEqual(sub(1,2),-1,'ok')
-		self.assertEqual(sub(-1,-2),1,'ok')
-		self.assertEqual(sub(-1,2),-3,'ok')
+		self.assertEqual(self.c.sub(2,1),1,'ok')
+		self.assertEqual(self.c.sub(1,2),-1,'ok')
+		self.assertEqual(self.c.sub(-1,-2),1,'ok')
+		self.assertEqual(self.c.sub(-1,2),-3,'Failed comparison')
 	def testmul(self):
-		self.assertEqual(mul(4,2),8,'ok')
-		self.assertEqual(mul(4,-2),-8,'ok')
-		self.assertEqual(mul(-2,-4),8,'ok')
-		self.assertEqual(mul(-4,2),-8,'ok')
+		self.assertEqual(self.c.mul(4,2),8,'ok')
+		self.assertEqual(self.c.mul(4,-2),-8,'ok')
+		self.assertEqual(self.c.mul(-2,-4),8,'ok')
+		self.assertEqual(self.c.mul(-4,2),-8,'ok')
 	def testdiv(self):
-		self.assertEqual(div(4,2),2,'ok')
-		self.assertEqual(div(-4,2),-2,'ok')
-if __name__=='__main__":
-unittest.main()
+		self.assertEqual(self.c.div(4,2),2,'ok')
+		self.assertEqual(self.c.div(-4,2),-2,'ok')
+if __name__== '__main__':
+		unittest.main()
