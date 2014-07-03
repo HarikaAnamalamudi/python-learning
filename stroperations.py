7d17680333a5c2915d679d31e7938ac929cf2e64
Aseem Shakuntal <aseem.shakuntal@codemenders.com>
2014-07-03 21:06:23 +0300

    Add some comments for the current class definitions

--- stroperations.py
+++ stroperations.py
@@ -1,5 +1,13 @@
 class Operation:
+    # aseem: please read the documentation of classes again.
+    # Q: When you define a class (for example in c++= and give
+    # some arguments in the constructor, how do you instantiate
+    # the class?
 	def _init(self,string):
+        # When you initially defined the object, what was the purpose
+        # of defining the __init__ ?
+        # Why did you define the init as def __init__(self, string): ?
+        # What is the purpose of the argument string?
 		self.string1=string1
 	def length(self,string):
 		return len(string)
@@ -8,6 +16,8 @@ class Operation:
 	def rep(self,string):
 		return string*2
 s=Operation()
+# In your current class definition, there is no constructor and so,
+# this works. What change you should do to initialize this object?
 print("choose an operation:")
 print("1:length of string:")
 print("2:concatenation of string:")
